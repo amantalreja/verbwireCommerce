41653a8cff22dd29ffaded75655525cf3e74a84a
Aman Talreja <64047297+amantalreja@users.noreply.github.com>
2023-06-14 15:29:58 +0000

    basetwo

--- marketplace/src/index.js
+++ marketplace/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Home from "./pages/Home";
+import Home from "./pages/App";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 
--- marketplace/src/pages/Layout.js
+++ marketplace/src/pages/Layout.js
@@ -1,20 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
-
+import './Layout.css';
 const Layout = () => {
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
+        <div> <Link to="/">Home</Link></div>
+        <div> <Link to="/blogs">Blogs</Link></div>
+        <div> <Link to="/contact">Contact</Link></div>
+
       </nav>
 
       <Outlet />
